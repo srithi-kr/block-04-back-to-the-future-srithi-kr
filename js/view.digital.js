f3f1e2e702da4d6d569693f74c06c5c2f86bd55c
srithi-kr <s2520892044@fhooe.at>
2026-04-30 20:13:27 +0200

    Add DigitalClockView class for displaying time

--- js/view.digital.js
+++ js/view.digital.js
@@ -1,1 +1,26 @@
 "use strict";
+
+class DigitalClockView {
+    constructor(elementId) {
+        this.element = document.getElementById(elementId);
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+    }
+
+    updateTime(hours, minutes, seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.render();
+    }
+
+    render() {
+        const timeString = `${this.formatNumber(this.hours)}:${this.formatNumber(this.minutes)}:${this.formatNumber(this.seconds)}`;
+        this.element.textContent = timeString;
+    }
+
+    formatNumber(num) {
+        return num < 10 ? '0' + num : num;
+    }
+}
